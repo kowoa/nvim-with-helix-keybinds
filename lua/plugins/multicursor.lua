9e0e16b4089c20b721778643ab403fcb94795da6
kowoa <koadahoakodah@gmail.com>
2023-10-03 18:11:22 -0400

    Add align keymap for multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -3,9 +3,11 @@ return {
   branch = 'master',
   init = function()
     vim.g.VM_default_mappings = 0;
+    vim.g.VM_leader = '\\';
     vim.g.VM_maps = {
       ['Add Cursor Down'] = 'C',
       ['Add Cursor Up'] = '<A-C>',
+      ['Align'] = '\\a',
     };
   end,
 }
